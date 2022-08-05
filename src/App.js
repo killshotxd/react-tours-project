d90d684c1d9968d2c26e9014de072f90890a13fa
killshotxd <hassanansari211@gmail.com>
2022-08-05 14:56:50 +0530

    updated own api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = "https://course-api.com/react-tours-project";
+const url = "https://tourhassanapi.herokuapp.com/api";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
